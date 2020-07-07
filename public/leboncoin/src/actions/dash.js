02863bc044d3ee0013f38ce6143cd3866c36014d
Amine ELMAROUFI <elmaroufi.amine@gmail.com>
2020-07-07 13:01:31 +0200

    add docker/nginx config

--- public/leboncoin/src/actions/dash.js
+++
@@ -1,6 +0,0 @@
-import * as ActionTypes from "../utils/types";
-
-export const getBookings = (params) => ({
-  type: ActionTypes.GET_BOOKINGS,
-  params,
-});
--- public/leboncoin/src/sagas/index.js
+++
@@ -1,6 +0,0 @@
-import { fork, all } from "redux-saga/effects";
-import watchDashRequest from "./dash";
-
-export default function* rootSaga() {
-  yield all([fork(watchDashRequest)]);
-}
